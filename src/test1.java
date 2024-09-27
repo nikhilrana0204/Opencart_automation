05172bb7e02c015e8b0e02e1e4d71c4155537eea
nikhilrana0204 <nikhilr989@gmail.com>
2024-09-27 07:52:42 -0400

    My first eclipse commit

---
+++ src/test1.java
@@ -0,0 +1,14 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String hungry = "ye";
+if (hungry == "yes")
+	System.out.println("eat something");
+else {
+	System.out.println("do work");
+}
+	}
+
+}
